49f9468db0eb3f872643481d71d2235777d30c7c
talissonsidnei <158642399+MarlonUTF@users.noreply.github.com>
2025-06-08 22:46:12 -0300

    Configurações de deploy

--- src/components/Grid/gridItem.jsx
+++ src/components/Grid/gridItem.jsx
@@ -8,9 +8,11 @@ export default function GridItem({ item, onDelete }) {
   return (
     <tr className="border-b border-gray-100 hover:bg-gray-100">
       <td className="py-2 px-4 text-sm text-gray-800">{item.desc}</td>
+      
       <td className="py-2 px-4 text-sm text-gray-800">
         R$ {parseFloat(item.amount).toFixed(2)}
       </td>
+
       <td className="py-2 px-4 text-center">
         {item.expense ? (
           <FaRegArrowAltCircleDown className="text-red-500 inline-block" />
@@ -18,6 +20,7 @@ export default function GridItem({ item, onDelete }) {
           <FaRegArrowAltCircleUp className="text-green-500 inline-block" />
         )}
       </td>
+
       <td className="py-2 px-4 text-center">
         <button
           onClick={() => onDelete(item.id)}
